caca5bf048d50a97cdeadf6a90e12b8082374f44
theperacar <petricpetar22@gmail.com>
2020-07-20 00:20:09 +0200

    finished function for making menu

--- src/navMenu.js
+++ src/navMenu.js
@@ -1,19 +1,21 @@
 // Module with Factory Function that creates menu
 
-const { NamedChunksPlugin } = require("webpack");
-
 let contentId;
 
-const vertMenuFac = (names, class, whereTo) => {
+const vertMenuFac = ([...names], clasa , whereTo) => {
   const parent = document.createElement('div')
   contentId = document.querySelector(whereTo);
   contentId.append(parent)
-  parent.classList.add(class);
+  parent.classList.add(clasa);
 
   for(let i = 0; i < names.length; i++) {
     // make menu tabs
-    const links = document.createElement('a href')
-    // append tabs to parent id 
-
+    const tabs = document.createElement('li');
+    const tabsTxt = document.createTextNode(names[i])
+    tabs.append(tabsTxt);
+    parent.append(tabs);
   }
+  return {names, clasa, whereTo}
 }
+
+export default vertMenuFac;
