66e297d7945664c087c11a799ffb42fb4b5dc572
theperacar <petricpetar22@gmail.com>
2020-07-23 20:24:13 +0200

    Finishing this page that looks like it is from 99'

--- src/contactPage.js
+++ src/contactPage.js
@@ -1,7 +1,38 @@
-const contactPagePart = (() =>{
+const contactPage = (() => {
   const contentId = document.querySelector(".content");
   contentId.textContent = "";
+  const contactPart = document.createElement('div');
+  contactPart.classList.add('contact');
+  contentId.append(contactPart);
+
+  contactPart.append(document.createTextNode("Contact Information"));
+  contactPart.append(document.createElement("br"));
+  const anotherDiv = document.createElement("div");
+
+  anotherDiv.classList.add("adress");
+
+  contactPart.append(anotherDiv);
+
+  const cityAd = document.createElement("div");
+  cityAd.classList.add("city")
+
+  contactPart.append(cityAd);
+
+  const emailAd = document.createElement("div");
+  emailAd.classList.add("emailAd")
+
+  contactPart.append(emailAd);
+
+  emailAd.append(document.createTextNode("baratie@rest.sa"));
+  emailAd.append(document.createElement("div"));
+  emailAd.append(document.createTextNode("(555)-442-385"));
+  
+  
+  cityAd.append(document.createTextNode("Pretoria 25300"))
+  
+  anotherDiv.append(document.createTextNode("350 Futurama Street"));
+
 
 });
 
-export default contactPagePart;
+export default contactPage;
--- src/navMenu.js
+++ src/navMenu.js
@@ -1,7 +1,7 @@
 // Module with Factory Function that creates menu
 import menuPage from './menu';
 import homePageRender from './homePage';
-import contactPagePart from './contactPage';
+import contactPage from './contactPage';
 
 let contentId;
 
@@ -26,7 +26,7 @@ const vertMenuFac = ([...names], clasa , whereTo) => {
         } else if (e.target.textContent == "Menu") {
           menuPage();
         } else if (e.target.textContent == "Contact") {
-          contactPagePart();
+          contactPage();
         }
       
       }
